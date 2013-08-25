a9dfa8fd2ee6adcc5b39394ac3469d5de7ec845e
Th3-Alch3mist <kkdthunlshd@gmail.com>
2013-08-25 07:04:11 +0530

    Update test.php
    A better test example with create table, select, insert, update etc.

--- test/test.php
+++ test/test.php
@@ -1,17 +1,69 @@
 <?php
 require('../src/PDOWrapper.php');
-$rand = rand();
-$pdo = PDOWrapper::instance();
-$pdo->configMaster('localhost', 'test', 'root', '');
-$pdo->configSlave('localhost', 'test', 'root', '');
-$pdo->insert('pdo_test', array(
-	'name' => 'bob'.$rand,
-	'email' => 'bob'.$rand.'@email.com'
-));
-$result = $pdo->select('pdo_test', null, 5, null, array('id'=>'DESC'));
+$db = PDOWrapper::instance();
 
-foreach($result as $key=>$value) {
-	echo "Name: ".$value['name']."<br />";
-	echo "Email: ".$value['email']."<br />";
-	echo "<hr />";
+$db->configMaster('localhost', 'test', 'root', '');
+$db->configSlave('localhost', 'test', 'root', '');
+
+
+echo $db->create("testing",
+                 array("id"    => "int not null auto_increment",
+                       "name"  => "varchar(20)",
+                       "email" => "varchar(20)",
+                       "stamp" => "timestamp not null default current_timestamp"),
+                       "id");
+
+for($i = 0; $i < 9; $i++) {
+  $rand = rand();
+  $db->insert('testing', 
+              array('name' => 'bob'.$rand,
+	                  'email' => 'bob'.$rand.'@email.com'));
 }
+
+$result = $db->select('testing',
+                      array("id", "name", "email"),
+                      array("id" => 1, "name" => "bob%", "email" => "%3%"),
+                      array(">", "LIKE", "LIKE"),
+                      array("AND","AND"),
+                      20,
+                      null,
+                      array('id'),
+                      array('id' => ''),
+                      false);
+
+
+echo $db->select_max("testing","id")."<br />";
+echo $db->select_min("testing","id",array("name" => "%3%"), array("LIKE"));
+
+print_r($db->rowcount('testing',
+                      null,
+                      array("id" => 1, "name" => "bob%", "email" => "%3%"),
+                      array(">", "LIKE", "LIKE"),
+                      array("AND","AND")));
+                      
+echo $db->update('testing',
+                 array("id" => 2),
+                 array("id" => 3));
+                 
+if(empty($result[0])) {
+  print_r($result);
+}
+else {
+  foreach($result as $key => $value) {
+    echo "Id: ".$value['id']."<br />";
+	  echo "Name: ".$value['name']."<br />";
+	  echo "Email: ".$value['email']."<br />";
+	  echo "<hr />";
+  }
+}
+
+echo $db->delete('testing',
+                 array("id" => 1, "name" => "bob%"),
+                 array("=", "LIKE"),
+                 array("AND"));
+
+print_r($db->query("SELECT COUNT(*) FROM testing WHERE name LIKE :name",array("name" => "%3%")));
+
+echo $db->truncate("testing");
+echo $db->drop("testing");
+?>
